35e7bbf67a0343f680d80134ed7299454bcde354
Kotova <kotova.mila23@gmail.com>
2023-11-07 18:57:35 +0300

    Added calculation of the average score for a homework 2.1.3 by modules.

---
+++ task_score.js
@@ -0,0 +1,16 @@
+let firstName = "Lyudmila"
+let lastName = "Kotova"
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
+console.log(`This is calculation of the average score for ${fullName} homework by modules:`)
+
+// расчет среднего балла за домашние задания студента по модулям
+let introduction = 90 
+let git = 199
+let js = 40
+
+let total = introduction + git + js
+
+
+console.log(total/3)
