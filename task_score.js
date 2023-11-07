459cf35c651b76da39d715100a3607822da531d9
Kotova <kotova.mila23@gmail.com>
2023-11-07 19:02:19 +0300

    used Prettier for beautifying  code

--- task_score.js
+++ task_score.js
@@ -1,16 +1,17 @@
-let firstName = "Lyudmila"
-let lastName = "Kotova"
-let fullName = firstName + " " + lastName
+let firstName = "Lyudmila";
+let lastName = "Kotova";
+let fullName = firstName + " " + lastName;
 
-console.log(fullName)
-console.log(`This is calculation of the average score for ${fullName} homework by modules:`)
+console.log(fullName);
+console.log(
+  `This is calculation of the average score for ${fullName} homework by modules:`
+);
 
 // расчет среднего балла за домашние задания студента по модулям
-let introduction = 90 
-let git = 199
-let js = 40
+let introduction = 90;
+let git = 199;
+let js = 40;
 
-let total = introduction + git + js
+let total = introduction + git + js;
 
-
-console.log(total/3)
+console.log(total / 3);
